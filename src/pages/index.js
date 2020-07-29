14586fe1c5c9de797736b1ac93a661cb5a1bdfbe
Danielkvist <danielkvist@protonmail.com>
2020-07-29 16:24:17 +0200

    feat(app): refactor index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { navigate } from "gatsby"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import Typography from "@material-ui/core/Typography"
+import Container from "@material-ui/core/Container"
+import Button from "@material-ui/core/Button"
+
+import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <div>
-      <Link to="/account">Go to your account</Link>
-    </div>
+    <Layout>
+      <CssBaseline />
+      <Container
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+        }}
+      >
+        <Typography variant="h1">Tasky</Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => navigate("/account")}
+        >
+          Login
+        </Button>
+      </Container>
+    </Layout>
   )
 }
 
