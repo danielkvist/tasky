03622724f8c4b731705f97eab2e049370e0d1a0d
Danielkvist <danielkvist@protonmail.com>
2020-07-29 16:23:06 +0200

    feat(app): add unauthenticated appbar component

---
+++ src/components/unauthenticated-appbar.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { navigate } from "gatsby"
+import { makeStyles } from "@material-ui/core/styles"
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
+
+const UnauthenticatedAppBar = () => {
+  const classes = useStyles()
+
+  return (
+    <nav className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Tasky
+          </Typography>
+          <Button color="inherit" onClick={() => navigate("/account")}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </nav>
+  )
+}
+
+export default UnauthenticatedAppBar
