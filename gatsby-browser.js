f0e9c78b00d7145bb22cf023ba5299166122f029
Danielkvist <danielkvist@protonmail.com>
2020-07-29 16:21:37 +0200

    fix(gatsby): remove styled components for auth err

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react"
-import { ThemeProvider } from "styled-components"
 import "normalize.css"
 
 import { silentAuth } from "./src/utils/auth"
-import Theme from "./src/theme/theme"
-import GlobalStyle from "./src/theme/global-style"
 
 const SessionCheck = ({ children }) => {
   const [loading, setLoading] = useState(true)
@@ -19,11 +16,5 @@ const SessionCheck = ({ children }) => {
 }
 
 export const wrapRootElement = ({ element }) => {
-  return (
-    <SessionCheck>
-      <ThemeProvider theme={Theme}>
-        <GlobalStyle>{element}</GlobalStyle>
-      </ThemeProvider>
-    </SessionCheck>
-  )
+  return <SessionCheck>{element}</SessionCheck>
 }
