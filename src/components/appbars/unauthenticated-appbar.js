8f4596c756ccf8d929b03749797565d3b01a23ac
Danielkvist <danielkvist@protonmail.com>
2020-08-01 18:42:25 +0200

    refactor(app): remove menu icon

--- src/components/appbars/unauthenticated-appbar.js
+++ src/components/appbars/unauthenticated-appbar.js
@@ -1,14 +1,7 @@
 import React from "react"
 import { navigate } from "gatsby"
 import { makeStyles } from "@material-ui/core/styles"
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  IconButton,
-} from "@material-ui/core"
-import MenuIcon from "@material-ui/icons/Menu"
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core"
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -29,14 +22,6 @@ const UnauthenticatedAppBar = () => {
     <nav className={classes.root}>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
             Tasky
           </Typography>
