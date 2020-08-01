0a4912fb7183c4aebe76faaf51c60428944b3538
Danielkvist <danielkvist@protonmail.com>
2020-08-01 18:42:00 +0200

    feat(app): add drawer to authenticated appbar

--- src/components/appbars/authenticated-appbar.js
+++ src/components/appbars/authenticated-appbar.js
@@ -1,68 +1,223 @@
-import React from "react"
+import React, { useState } from "react"
 import { navigate } from "gatsby"
-import { makeStyles } from "@material-ui/core/styles"
+import clsx from "clsx"
 import {
+  makeStyles,
+  useTheme,
   AppBar,
+  Button,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   Toolbar,
   Typography,
-  Button,
-  IconButton,
 } from "@material-ui/core"
 import MenuIcon from "@material-ui/icons/Menu"
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"
+import ChevronRightIcon from "@material-ui/icons/ChevronRight"
 import NotificationsNone from "@material-ui/icons/NotificationsNone"
 import SettingsIcon from "@material-ui/icons/Settings"
+import InboxIcon from "@material-ui/icons/MoveToInbox"
+import StarBorderIcon from "@material-ui/icons/StarBorder"
+import TodayIcon from "@material-ui/icons/Today"
+import WbSunnyIcon from "@material-ui/icons/WbSunny"
+import ViewWeekIcon from "@material-ui/icons/ViewWeek"
 
 import { logout } from "../../utils/auth"
 
+const drawerWidth = 240
+
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  buttonGroup: {
+    marginLeft: "auto",
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: 36,
   },
-  title: {
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  },
+  content: {
     flexGrow: 1,
+    padding: theme.spacing(3),
   },
 }))
 
 const AuthenticatedAppBar = () => {
   const classes = useStyles()
+  const theme = useTheme()
+  const [open, setOpen] = useState(false)
+
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
 
   return (
-    <nav className={classes.root}>
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Tasky
-          </Typography>
-          <Button color="inherit" onClick={() => logout()}>
-            Log Out
-          </Button>
+    <>
+      <nav className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Tasky
+            </Typography>
 
-          <IconButton aria-label="Notifications" edge="end" color="inherit">
-            <NotificationsNone />
-          </IconButton>
+            <div className={classes.buttonGroup}>
+              <Button color="inherit" onClick={() => logout()}>
+                Log Out
+              </Button>
 
-          <IconButton
-            aria-label="Display more actions"
-            edge="end"
-            color="inherit"
-            onClick={() => navigate("/accounts/settings")}
-          >
-            <SettingsIcon />
+              <IconButton aria-label="Notifications" edge="end" color="inherit">
+                <NotificationsNone />
+              </IconButton>
+
+              <IconButton
+                aria-label="Display more actions"
+                edge="end"
+                color="inherit"
+                onClick={() => navigate("/accounts/settings")}
+              >
+                <SettingsIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </nav>
+
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
           </IconButton>
-        </Toolbar>
-      </AppBar>
-    </nav>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+
+          <ListItem button>
+            <ListItemIcon>
+              <StarBorderIcon />
+            </ListItemIcon>
+            <ListItemText primary="Important" />
+          </ListItem>
+
+          <ListItem button>
+            <ListItemIcon>
+              <TodayIcon />
+            </ListItemIcon>
+            <ListItemText primary="Today" />
+          </ListItem>
+
+          <ListItem button>
+            <ListItemIcon>
+              <WbSunnyIcon />
+            </ListItemIcon>
+            <ListItemText primary="Tomorrow" />
+          </ListItem>
+
+          <ListItem button>
+            <ListItemIcon>
+              <ViewWeekIcon />
+            </ListItemIcon>
+            <ListItemText primary="Week" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
   )
 }
 
