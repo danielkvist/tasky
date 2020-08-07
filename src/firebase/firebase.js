c77352c65b144707062f254750a6635e09e5f97f
Danielkvist <danielkvist@protonmail.com>
2020-08-07 11:21:05 +0200

    feat(app): add firestore
    Add Firestore to Firebase and add create and delete methods.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,10 @@
 import { createContext } from "react"
 import firebase from "firebase/app"
 import "firebase/auth"
+import "firebase/firestore"
+import faker from "faker"
+
+import Task from "../models/tasks"
 
 const isBrowser = typeof window !== "undefined"
 
@@ -8,20 +12,57 @@ class Firebase {
   constructor(config) {
     if (!isBrowser) return null
     firebase.initializeApp(config)
+
     this.auth = firebase.auth()
+    this.db = firebase.firestore()
   }
 
-  auth = () => this.auth
-
+  // AUTH
   login = async () => {
     const googleProvider = new firebase.auth.GoogleAuthProvider()
-
     this.auth.signInWithPopup(googleProvider).catch(e => console.error(e))
   }
 
   logout = async () => {
+    // TODO: Clean up
+    // TODO: Unsuscribe
     this.auth.signOut().catch(e => console.error(e))
   }
+
+  // FIRESTORE
+  createTask = task => {
+    const document = this.db
+      .collection("users")
+      .doc(this.auth.currentUser.uid)
+      .collection("tasks")
+      .doc()
+
+    const documentUuid = document.id
+
+    document
+      .set({
+        ...Task,
+        ...task,
+        id: documentUuid,
+        title: faker.lorem.sentence(),
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then(ref => console.log(ref))
+      .catch(e => console.error(e))
+  }
+
+  deleteTask = async taskId => {
+    const res = await this.db
+      .collection("users")
+      .doc(this.auth.currentUser.uid)
+      .collection("tasks")
+      .doc(taskId)
+      .delete()
+
+    console.log(res)
+  }
+
+  updateTask = task => {}
 }
 
 const FirebaseCtx = createContext(null)
