ede9ad27102abe0a2187693a83662d5157ea57ac
Danielkvist <danielkvist@protonmail.com>
2020-08-07 11:19:39 +0200

    feat(app): add task model

---
+++ src/models/tasks.js
@@ -0,0 +1,14 @@
+const Task = {
+  id: "",
+  title: "",
+  description: "",
+  done: false,
+  important: false,
+  repeat: false,
+  repeatEach: "",
+  dueDate: null,
+  remind: false,
+  project: "",
+}
+
+export default Task
